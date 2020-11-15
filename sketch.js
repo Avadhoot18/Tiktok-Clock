ddbf3d1377a6794e7cce62e1232c24e6bd06f1f0
Avadhoot18 <69972926+Avadhoot18@users.noreply.github.com>
2020-11-15 10:58:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+var hr,min,sec;
+var hourAngle, minuteAngle, secondAngle;
+
+function setup() {
+  createCanvas(400,400);
+  angleMode(degrees);
+}
+
+function draw() {
+  background(0);
+  translate(200,200);
+  rotate(-90);
+
+  hr = hour();
+  min = minute();
+  sec = second();
+
+  secondAngle = map(sec,0,60,0,360);
+  minuteAngle = map(min,0,60,0,360);
+  hourAngle = map(hr%12,0,12,0,360);
+
+  push();
+  rotate(secondAngle);
+  stroke(255,0,0);
+  strokeWeight(7);
+  line(0,0,100,0);
+  pop();
+
+  push();
+  rotate(minuteAngle);
+  stroke(0,255,0);
+  strokeWeight(7);
+  line(0,0,75,0);
+  pop();
+
+  push();
+  rotate(hourAngle);
+  stroke(0,0,255);
+  strokeWeight(7);
+  line(0,0,50,0);
+  pop();
+
+  stroke(255,0,255);
+  point(0,0);
+
+  strokeWeight(10);
+  stroke(255,0,0);
+  noFill();
+  arc(0,0,300,300,0,secondAngle);
+
+  strokeWeight(10);
+  stroke(0,255,0);
+  noFill();
+  arc(0,0,280,280,0,minuteAngle);
+
+  strokeWeight(10);
+  stroke(0,0,255);
+  noFill();
+  arc(0,0,260,260,0,hourAngle);
+}
